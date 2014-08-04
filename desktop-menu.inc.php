8f01167b995064bc1b58092715ad77a368e219f8
Errol Sidelsky <errol@squie.com>
2014-08-04 12:32:19 +0100

    Menu text included

---
+++ desktop-menu.inc.php
@@ -0,0 +1,127 @@
+<div class="menu" id="desktop">
+  <nav class="main-navigation inner">
+   <ul class="sf-menu" >
+    <!-- Travel -->
+    <li class="travel current">
+     <a href="http://www.hotels.com">D Travel tools</a>
+     <div class="sf-mega menu-wrapper">
+      <ul class="sub-menu">
+       <li>
+        <a href="#">Plan your journey</a>
+        <i class="alpha"></i>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      </li>
+      <li>
+        <a href="#">Find your nearest</a>
+        <i class="bravo"></i>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      </li>
+    </ul>
+  </div>
+</li>
+<!-- Work -->
+<li class="work">
+ <a href="#">Work</a>
+ <div class="sf-mega menu-wrapper">
+  <ul class="sub-menu">
+   <li class="first">
+    <a href="#">Smarter commuting</a>
+    <i class="alpha"></i>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+  </li>
+  <li>
+    <a href="#">Smarter businesses</a>
+    <i class="bravo"></i>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+  </li>
+</ul>
+<!-- Sub sub menus-->
+<ul class="sub-menu menu">
+ <li>
+  <a href="#">Walking</a>
+</li>
+<li>
+  <a href="#">Cycling</a>
+</li>
+<li>
+  <a href="#">Public transport</a>
+</li>
+<li>
+  <a href="#">Driving</a>
+</li>
+</ul>
+<ul class="sub-menu menu">
+ <li>
+  <a href="#">Why GoSmarter?</a>
+</li>
+<li>
+  <a href="#">What’s on offer?</a>
+</li>
+<li>
+  <a href="#">How to get involved</a>
+</li>
+<li>
+  <a href="#">Accreditation Awards</a>
+</li>
+</ul>
+</div>
+</li>
+<!-- School -->
+<li class="school">
+ <a href="#">School</a>
+ <div class="sf-mega menu-wrapper">
+  <ul class="sub-menu">
+   <li class="first">
+    <a href="#">Smarter journeys</a>
+    <i class="alpha"></i>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+  </li>
+  <li>
+    <a href="#">Smarter schools</a>
+    <i class="bravo"></i>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+  </li>
+</ul>
+<!-- Sub sub menus-->
+<ul class="sub-menu menu">
+ <li>
+  <a href="#">Walking</a>
+</li>
+<li>
+  <a href="#">Cycling</a>
+</li>
+<li>
+  <a href="#">Public transport</a>
+</li>
+<li>
+  <a href="#">Road safety</a>
+</li>
+<li>
+  <a href="#">Infrastructure improvements</a>
+</li>
+</ul>
+<ul class="sub-menu menu">
+ <li>
+  <a href="#">Why GoSmarter?</a>
+</li>
+<li>
+  <a href="#">What’s on offer?</a>
+</li>
+<li>
+  <a href="#">How to get involved</a>
+</li>
+</ul>
+</div>
+</li>
+<!-- Jobseekers -->
+<li class="jobseekers">
+ <a href="#">Jobseekers</a>
+</li>
+<!-- Blog -->
+<li class="blog">
+ <a href="http://www.wordpress.com">News and events</a>
+</li>
+</ul>
+</nav>
+</div>
+<!-- End desktop menu --> 
---
+++ mobile-menu.inc.php
@@ -0,0 +1,101 @@
+<div class="menu" id="mobile">
+  <nav class="main-navigation zinner">
+   <ul class="sf-menu">
+    <!-- Travel -->
+    <li class="travel">
+     <a href="http://www.hotels.com">M Travel tools</a>
+     <ul class="sub-menu">
+      <li>
+       <a href="#">Plan your journey</a>
+       <i class="alpha"></i>
+     </li>
+     <li>
+       <a href="#">Find your nearest</a>
+       <i class="bravo"></i>
+     </li>
+   </ul>
+ </li>
+ <!-- Work -->
+ <li class="work">
+   <a href="#">Work</a>
+   <ul class="sub-menu">
+    <li class="first">
+     <a href="#">Getting to work</a>
+     <i class="alpha"></i>
+     <ul class="sub-inner-menu">
+      <li>
+       <a href="#">Walking</a>
+     </li>
+     <li>
+       <a href="#">Cycling</a>
+     </li>
+     <li>
+       <a href="#">Public transport</a>
+     </li>
+     <li>
+       <a href="#">Road safety</a>
+     </li>
+     <li>
+       <a href="#">Infrastructure improvements</a>
+     </li>
+   </ul>
+ </li>
+ <li class="first">
+   <a href="#">Smarter businesses</a>
+   <i class="bravo"></i>
+   <ul class="sub-inner-menu">
+    <li>
+     <a href="#">Why GoSmarter?</a>
+   </li>
+   <li>
+     <a href="#">What’s on offer?</a>
+   </li>
+   <li>
+     <a href="#">How to get involved</a>
+   </li>
+ </ul>
+</li>
+</ul>
+</li>
+<!-- School -->
+<li class="school">
+ <a href="#">School</a>
+ <ul class="sub-menu">
+  <li class="first">
+   <a href="#">Smarter journeys</a>
+   <i class="alpha"></i>
+   <ul class="sub-inner-menu">
+    <li>
+     <a href="#">Walking</a>
+   </li>
+   <li>
+     <a href="#">Cycling</a>
+   </li>
+   <li>
+     <a href="#">Public transport</a>
+   </li>
+   <li>
+     <a href="#">Road safety</a>
+   </li>
+   <li>
+     <a href="#">Infrastructure improvements</a>
+   </li>
+ </ul>
+</li>
+<li class="first">
+ <a href="#">Smarter schools</a>
+ <i class="bravo"></i>
+ <ul class="sub-inner-menu">
+  <li>
+   <a href="#">Why GoSmarter?</a>
+ </li>
+ <li>
+   <a href="#">What’s on offer?</a>
+ </li>
+ <li>
+   <a href="#">How to get involved</a>
+ </li>
+</ul>
+</li>
+</ul>
+</li>
