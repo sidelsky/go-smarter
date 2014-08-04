94bab2ebb13e8aa2c320a4e414497aa0617f0e43
Errol Sidelsky <errol@squie.com>
2014-08-04 12:39:15 +0100

    Headers included

---
+++ header.inc.php
@@ -0,0 +1,38 @@
+<header class="header">
+ <!-- Hamburger icon -->
+ <div class="menu-cliccato" id="ham">
+  <div class="bar Sx_copy_5"></div>
+  <div class="bar Sx_copy_4"></div>
+  <div class="bar Sx_copy_3"></div>
+</div>
+
+<div class="inner">
+  <div class="logo-container">
+    <a href="#">
+      <img src="img/gosmarter-logo.png" alt="Go Smarter" class="logo">
+    </a>
+  </div>
+  <form action="">
+    <input type="text" name="" value="" placeholder="Search">
+  </form>
+</div>
+
+<!-- Desktop menu -->
+<?php include 'desktop-menu.inc.php' ?>	
+
+<!-- Mobile menu -->
+<?php include 'mobile-menu.inc.php' ?> 
+
+<!-- Jobseekers -->
+<li class="jobseekers">
+ <a href="#">Jobseekers</a>
+</li>
+<!-- Blog -->
+<li class="blog">
+ <a href="http://www.wordpress.com">News and events</a>
+</li>
+</ul>
+</nav>
+</div>
+<!-- End mobile menu --> 	
+</header>
